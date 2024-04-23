fdef38bb85d6b38eb100b6fd7fbab5e5c5b89a57
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-04-23 15:19:10 -0300

    refactor: import movement for module export

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
