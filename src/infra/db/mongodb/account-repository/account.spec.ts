09b510a21669dcbbf3c6e077d636cc309b69a8e0
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-04-25 13:59:40 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,8 +10,12 @@
 //     await MongoHelper.disconnect()
 //   })
 
+//   const makeSut = (): AccountMongoRepository => {
+//     return new AccountMongoRepository()
+//   }
+
 //   test('Should return an account on success', async () => {
-//     const sut = new AccountMongoRepository()
+//     const sut = makeSut()
 //     const account = await sut.createAccount({
 //       name: 'any_name',
 //       email: 'any_email@mail.com',
