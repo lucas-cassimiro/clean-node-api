a938d51af18204cd60a531bfe10c3ef31f416473
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-05-09 14:16:16 -0300

    refactor: change import

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
-export * from '../../../presentation/protocols'
-export * from '../../../presentation/protocols/email-validator'
+export * from '../../protocols'
+export * from '../../protocols/email-validator'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
